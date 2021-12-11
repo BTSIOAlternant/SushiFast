2a1d16e9c2fd088a0cc08910a90355e372d4c019
= <=>
2021-12-11 01:32:05 +0100

    Add Delete ALL Panier + Check if 2 3 4 value same + some fix

--- src/app/component/panier/panier.component.ts
+++ src/app/component/panier/panier.component.ts
@@ -1,7 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { Commande } from '../../classes/commande';
-import { CommandeService } from '../../services/commande.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Commande
+} from '../../classes/commande';
+import {
+  CommandeService
+} from '../../services/commande.service';
+import {
+  FormControl,
+  FormGroup
+} from '@angular/forms';
+import {
+  ValueConverter
+} from '@angular/compiler/src/render3/view/template';
 
 @Component({
   selector: 'app-panier',
@@ -14,26 +27,40 @@ export class PanierComponent implements OnInit {
   lesCommandes: Commande[];
   deleteArticle!: FormGroup;
 
-    constructor(private commandeService: CommandeService) {
+  constructor(private commandeService: CommandeService) {
     this.lesCommandes = this.commandeService.getAllItems();
     this.nbOflesCommandes = this.lesCommandes.length;
-   }
+  }
 
   ngOnInit(): void {
     this.deleteArticle = new FormGroup({
       nom: new FormControl('')
     });
+    this.checkAllsushi()
   }
+
+  checkAllsushi() {
+    const objets = this.lesCommandes.map(value => value.nom) 
+    const panier: Map<string, number> = new Map();
+    objets.forEach(objet => (panier.set(objet, (panier.get(objet)  || 0) + 1)))
+    console.log(panier);
+  }
+  
 
   supprimer(nom: string) {
     let storedNames = JSON.parse(localStorage.getItem('Commande :') || '[]');
-    for (let x = 0; x < storedNames.length; x++){
-      if(storedNames[x].nom.includes(nom)) {
+    for (let x = 0; x < storedNames.length; x++) {
+      if (storedNames[x].nom.includes(nom)) {
         storedNames.splice(x, 1);
       }
     }
     storedNames = JSON.stringify(storedNames);
     localStorage.setItem('Commande :', storedNames);
+    window.location.reload();
+  }
+
+  supprimerAll() {
+    localStorage.removeItem('Commande :');
     window.location.reload();
   }
 }
